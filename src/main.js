defec4741f3c3a6e621199bb89b9f85358ce1715
daphnevilhar <vilhardaphne@gmail.com>
2023-02-15 17:36:09 -0300

    Projeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+const botaoInicio = document.getElementbyId("botao-inicio");
+
+botaoInicio.addEventListener(click, function(){
+    location.href = "index2.html";
+})
