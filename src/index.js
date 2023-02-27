5647dcf073caf6f4ff5bc1ab3fff42cd9f4659ea
daphnevilhar <vilhardaphne@gmail.com>
2023-02-27 13:29:48 -0300

    Projeto data Js
    Co-authored-by: bea <bsoaresf8@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const botaoInicio = document.querySelector("#botao-inicio");
+
+botaoInicio.addEventListener("click", function(){
+  location.href = "index2.html";
+})
--- src/main.js
+++ src/main.js
@@ -5,8 +5,33 @@ import data from './data/ghibli/ghibli.js';
 
 console.log(example, data);
 
-const botaoInicio = document.querySelector("#botao-inicio");
 
-botaoInicio.addEventListener("click", function(){
-    location.href = "index2.html";
-})
+//console.log(data.films[0].people[0].name)
+
+/*for (let i=0; i < data.films.length ; i++) {
+  console.log(data.films[i].title);
+  const mostrarCard = document.querySelector(".cards");
+  mostrarCard.innerHTML += data.films[i].title
+}
+
+const cards = data.films
+
+function imagensDosFilmes(){
+  document.querySelector(".cards").innerHTML
+}
+
+cards.map(imagensDosFilmes)
+*/
+
+const mostrarCard = document.querySelector(".cards");
+
+function showCards(listaDeFilmes){
+  listaDeFilmes.forEach(film => {
+    mostrarCard.innerHTML += `
+    <div class="card">
+    <image class="card__image" src="${film.poster}">
+    <h3 class="card__title">${film.title}</h3>`
+  });
+}
+
+showCards(data.films);
